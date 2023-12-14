f159b69bd2ed2ec280bdd42cf0dc00859a3cb72d
kimzoo2 <kimzoo1207@gmail.com>
2023-12-14 18:57:20 +0900

    first commit

---
+++ src/test/java/com/flab/dduikka/DduikkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.dduikka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DduikkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
