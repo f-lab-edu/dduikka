ea0fecab66dd815cde37a351bccc95d55d1008fe
kimzoo2 <kimzoo1207@gmail.com>
2024-01-27 02:43:31 +0900

    Chore: 테스트 DB 데이터 추가

--- src/main/resources/h2/data.sql
+++ src/main/resources/h2/data.sql
@@ -1,2 +1,13 @@
 insert into vote(vote_id, vote_date, created_at)
 values (1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+insert into vote_record(vote_record_id, vote_id, user_id, vote_type, is_canceled, created_at)
+values (1, 1, 1, 'RUN', 1, CURRENT_TIMESTAMP);
+insert into vote_record(vote_record_id, vote_id, user_id, vote_type, is_canceled, created_at)
+values (2, 1, 2, 'NORUN', 0, CURRENT_TIMESTAMP);
+insert into vote_record(vote_record_id, vote_id, user_id, vote_type, is_canceled, created_at)
+values (3, 1, 3, 'RUN', 0, CURRENT_TIMESTAMP);
+insert into vote_record(vote_record_id, vote_id, user_id, vote_type, is_canceled, created_at)
+values (4, 1, 4, 'NORUN', 0, CURRENT_TIMESTAMP);
+insert into vote_record(vote_record_id, vote_id, user_id, vote_type, is_canceled, created_at)
+values (5, 1, 1, 'RUN', 0, CURRENT_TIMESTAMP);
