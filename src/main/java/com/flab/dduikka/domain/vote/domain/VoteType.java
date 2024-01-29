ed545b366cd801ad1097cae177a21b8bb113689b
kimzoo2 <kimzoo1207@gmail.com>
2024-01-29 16:13:22 +0900

    Refactor: VoteType getter 코드 제거
    - 롬복의 @Getter로 변경

--- src/main/java/com/flab/dduikka/domain/vote/domain/VoteType.java
+++ src/main/java/com/flab/dduikka/domain/vote/domain/VoteType.java
@@ -1,16 +1,13 @@
 package com.flab.dduikka.domain.vote.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum VoteType {
 	RUN("뛴다"),
 	NORUN("안뛴다");
 
 	private final String desc;
-
-	VoteType(String desc) {
-		this.desc = desc;
-	}
-
-	public String getDesc() {
-		return desc;
-	}
 }
