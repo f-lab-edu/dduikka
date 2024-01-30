f324bdeed31c3727ee8718101fd3e713ff7f6c9e
kimzoo2 <kimzoo1207@gmail.com>
2024-01-31 01:07:18 +0900

    Refactor: VoteRecordService의 비즈니스 로직을 일급컬렉션으로 변경
    - VoteRecordService의 비즈니스 로직을 VoteRecords 클래스로 리팩토링

--- src/main/java/com/flab/dduikka/domain/vote/application/VoteRecordService.java
+++ src/main/java/com/flab/dduikka/domain/vote/application/VoteRecordService.java
@@ -1,9 +1,6 @@
 package com.flab.dduikka.domain.vote.application;
 
 import java.time.LocalDate;
-import java.util.EnumMap;
-import java.util.List;
-import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
@@ -11,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.flab.dduikka.domain.vote.domain.Vote;
 import com.flab.dduikka.domain.vote.domain.VoteRecord;
-import com.flab.dduikka.domain.vote.domain.VoteType;
+import com.flab.dduikka.domain.vote.domain.VoteRecords;
 import com.flab.dduikka.domain.vote.dto.VoteRecordAddRequestDto;
 import com.flab.dduikka.domain.vote.dto.VoteRecordResponseDto;
 import com.flab.dduikka.domain.vote.dto.VoteResponseDto;
@@ -47,8 +44,8 @@ public class VoteRecordService {
 	public VoteResponseDto findVoteTypeCount(LocalDate voteDate) {
 		Vote foundVote = voteRepository.findByDate(voteDate)
 			.orElseThrow(() -> new NoSuchElementException("해당 날짜에 투표가 존재하지 않습니다. date" + voteDate));
-		List<VoteRecord> voteList = voteRecordRepository.findAllByVoteDate(voteDate);
-		return VoteResponseDto.toDto(foundVote, countVoteTypes(voteList));
+		VoteRecords voteRecords = new VoteRecords(voteRecordRepository.findAllByVoteDate(voteDate));
+		return VoteResponseDto.toDto(foundVote, voteRecords.countsVoteRecords());
 	}
 
 	public void cancelVoteRecord(long voteRecordId) {
@@ -56,21 +53,5 @@ public class VoteRecordService {
 			.orElseThrow(() -> new NoSuchElementException("존재하지 않는 투표 기록입니다. voteRecordId" + voteRecordId));
 		foundVoteRecord.cancel();
 		voteRecordRepository.cancelVoteRecord(foundVoteRecord);
-	}
-
-	/**
-	 * 조회한 투표 기록을 voteType 별 투표수를 담은 map으로 반환한다.
-	 * @param voteRecords
-	 * @return Map<VoteType, Integer>
-	 */
-	private Map<VoteType, Integer> countVoteTypes(List<VoteRecord> voteRecords) {
-		Map<VoteType, Integer> voteTypeCountMap = new EnumMap<>(VoteType.class);
-		for (VoteType voteType : VoteType.values()) {
-			voteTypeCountMap.put(voteType, 0);
-		}
-		for (VoteRecord voteRecord : voteRecords) {
-			voteTypeCountMap.put(voteRecord.getVoteType(), voteTypeCountMap.get(voteRecord.getVoteType()) + 1);
-		}
-		return voteTypeCountMap;
 	}
 }
