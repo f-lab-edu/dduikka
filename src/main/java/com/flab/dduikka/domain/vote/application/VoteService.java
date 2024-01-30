c861c9b23ee8addb757b20e9dfc1095008a935dc
kimzoo2 <kimzoo1207@gmail.com>
2024-01-30 15:26:42 +0900

    Refactor: VoteService scheduler 책임 분리
    - VoteService에 scheduler 책임을 VoteScheduler로 분리
    - application-local.yml에서 cron 표현식 관리하도록 변경

--- src/main/java/com/flab/dduikka/domain/vote/application/VoteService.java
+++ src/main/java/com/flab/dduikka/domain/vote/application/VoteService.java
@@ -2,7 +2,6 @@ package com.flab.dduikka.domain.vote.application;
 
 import java.time.LocalDate;
 
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.flab.dduikka.domain.vote.domain.Vote;
@@ -15,15 +14,6 @@ import lombok.RequiredArgsConstructor;
 public class VoteService {
 
 	private final VoteRepository voteRepository;
-
-	// TODO : 테스트 진행 필요
-	@Scheduled(cron = "0 0 23 * * *")
-	public void runSchedule() {
-		LocalDate nextDate = LocalDate
-			.now()
-			.plusDays(1);
-		createVote(nextDate);
-	}
 
 	public void createVote(LocalDate voteDate) {
 		Vote newVote = new Vote(voteDate);
