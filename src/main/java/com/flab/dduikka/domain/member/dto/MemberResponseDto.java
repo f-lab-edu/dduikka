22ba7b7109bd22462f3c405c47c2b63659336e1d
kimzoo2 <kimzoo1207@gmail.com>
2024-02-09 16:17:45 +0900

    Refactor: 회원 조회 응답 dto 회원 상태(MemberStatus) 제거

--- src/main/java/com/flab/dduikka/domain/member/dto/MemberResponseDto.java
+++ src/main/java/com/flab/dduikka/domain/member/dto/MemberResponseDto.java
@@ -3,7 +3,6 @@ package com.flab.dduikka.domain.member.dto;
 import java.time.LocalDate;
 
 import com.flab.dduikka.domain.member.domain.Member;
-import com.flab.dduikka.domain.member.domain.MemberStatus;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,14 +14,12 @@ public class MemberResponseDto {
 	private final Long memberId;
 	private final String email;
 	private final LocalDate joinDate;
-	private final MemberStatus memberStatus;
 
 	public static MemberResponseDto toDto(Member member) {
 		return new MemberResponseDto(
 			member.getMemberId(),
 			member.getEmail(),
-			member.getJoinDate(),
-			member.getMemberStatus()
+			member.getJoinDate()
 		);
 	}
 }
