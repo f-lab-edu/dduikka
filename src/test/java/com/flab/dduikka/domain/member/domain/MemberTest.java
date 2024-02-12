cdf4a2cef3ab8fa2da2ff18bbd600260488bccc0
kimzoo2 <kimzoo1207@gmail.com>
2024-02-13 01:54:43 +0900

    Test: 도메인 유효성 검증 test 추가

--- src/test/java/com/flab/dduikka/domain/member/domain/MemberTest.java
+++ src/test/java/com/flab/dduikka/domain/member/domain/MemberTest.java
@@ -39,4 +39,67 @@ class MemberTest {
 
 		assertThat(isJoined).isFalse();
 	}
+
+	@Test
+	void 가입일자가_미래면_예외를_발생시킨다() {
+		LocalDate tomorrow = LocalDate.now().plusDays(10);
+
+		assertThatThrownBy(
+			() -> Member.builder()
+				.memberId(1L)
+				.email("dduikka@dduikka.com")
+				.password("1234")
+				.memberStatus(MemberStatus.JOIN)
+				.joinDate(tomorrow)
+				.createAt(LocalDateTime.now())
+				.build()
+		).isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("%s는 미래날짜일 수 없습니다.".formatted("joinDate"));
+	}
+
+	@Test
+	void 이메일을_입력하지_않으면_예외가_발생한다() {
+		assertThatThrownBy(
+			() -> Member.builder()
+				.memberId(1L)
+				// .email("dduikka@dduikka.com")
+				.password("1234")
+				.memberStatus(MemberStatus.JOIN)
+				.joinDate(LocalDate.now())
+				.createAt(LocalDateTime.now())
+				.build()
+		).isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("%s는 null 또는 공백일 수 없습니다.".formatted("email"));
+	}
+
+	@Test
+	void 비밀번호를_입력하지_않으면_예외가_발생한다() {
+		assertThatThrownBy(
+			() -> Member.builder()
+				.memberId(1L)
+				.email("dduikka@dduikka.com")
+				// .password("1234")
+				.memberStatus(MemberStatus.JOIN)
+				.joinDate(LocalDate.now())
+				.createAt(LocalDateTime.now())
+				.build()
+		).isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("%s는 null 또는 공백일 수 없습니다.".formatted("password"));
+	}
+
+	@Test
+	void 멤버_상태를_입력하지_않으면_예외가_발생한다() {
+		assertThatThrownBy(
+			() -> Member.builder()
+				.memberId(1L)
+				.email("dduikka@dduikka.com")
+				.password("1234")
+				// .memberStatus(MemberStatus.JOIN)
+				.joinDate(LocalDate.now())
+				.createAt(LocalDateTime.now())
+				.build()
+		).isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("%s는 null일 수 없습니다.".formatted("memberStatus"));
+	}
+
 }
