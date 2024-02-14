16ac27e9c840fa86f904f775812da56f1e62dd2e
kimzoo2 <kimzoo1207@gmail.com>
2024-02-14 15:41:50 +0900

    Test: Validator 구현체를 통한 테스트로 변경(Mocking 제거)
    - 유효성 검증 테스트 시 Mocking을 제거하고 실객체로 테스트 하도록 변경했습니다.
    - 멤버 테스트에 포함된 유효성 검증 테스트를 제거하였습니다.

--- src/test/java/com/flab/dduikka/common/validator/CustomValidatorTest.java
+++ src/test/java/com/flab/dduikka/common/validator/CustomValidatorTest.java
@@ -1,72 +1,135 @@
 package com.flab.dduikka.common.validator;
 
 import static org.assertj.core.api.BDDAssertions.*;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.BDDMockito.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.Set;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.flab.dduikka.domain.member.domain.Member;
 import com.flab.dduikka.domain.member.domain.MemberStatus;
 
-import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
 import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 
-@ExtendWith(MockitoExtension.class)
 class CustomValidatorTest {
-	@InjectMocks
+
+	private static final String notNullErrorMessage = "널이어서는 안됩니다.";
+	private static final String notBlankErrorMessage = "공백일 수 없습니다.";
+	private static final String emailErrorMessage = "올바른 형식의 이메일 주소여야 합니다.";
+	private static final String pastOrPresentErrorMessage = "과거 또는 현재의 날짜여야 합니다.";
+
 	private CustomValidator customValidator;
-	@Mock
-	private Validator validator;
+
+	@BeforeEach
+	void setUp() {
+		ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+		Validator validator = validatorFactory.getValidator();
+		customValidator = new CustomValidator(validator);
+	}
 
 	@Test
 	void 객체의_유효성을_검증한다() {
 		//given
-		long memberId = 1L;
-		Set<ConstraintViolation<Object>> mockViolation
-			= Collections.emptySet();
 		Member mockMember = Member.builder()
-			.memberId(memberId)
+			.memberId(1L)
 			.email("test@dduikka.com")
 			.password("1234")
 			.memberStatus(MemberStatus.LEAVE)
 			.joinDate(LocalDate.now())
 			.createAt(LocalDateTime.now())
 			.build();
-		given(validator.validate(any())).willReturn(mockViolation);
 
 		//when
 		customValidator.validateObject(mockMember);
 
 		//then
-		verify(validator, times(1)).validate(mockMember);
 	}
 
 	@Test
-	void 객체의_유효성_검증이_실패하면_예외를_발생한다() {
+	void notBlank인_필드에_null이나_공백이_들어가면_예외를_발생한다() {
 		//given
-		long memberId = 1L;
 		Member mockMember = Member.builder()
-			.memberId(memberId)
-			.email("test@dduikka.com")
+			.memberId(1L)
+			// .email("test@dduikka.com")
 			.password("1234")
-			.memberStatus(MemberStatus.LEAVE)
+			.memberStatus(MemberStatus.JOIN)
 			.joinDate(LocalDate.now())
 			.createAt(LocalDateTime.now())
 			.build();
-		given(validator.validate(any())).willThrow(IllegalStateException.class);
+		Member anotherMember = Member.builder()
+			.memberId(2L)
+			.email("")
+			.password("1234")
+			.memberStatus(MemberStatus.JOIN)
+			.joinDate(LocalDate.now())
+			.createAt(LocalDateTime.now())
+			.build();
 
 		//when, then
-		thenThrownBy(() -> customValidator.validateObject(mockMember))
-			.isInstanceOf(IllegalStateException.class);
+		assertThatThrownBy(() -> customValidator.validateObject(mockMember))
+			.isInstanceOf(IllegalStateException.class)
+			.hasMessageContaining(notBlankErrorMessage);
+		assertThatThrownBy(() -> customValidator.validateObject(anotherMember))
+			.isInstanceOf(IllegalStateException.class)
+			.hasMessageContaining(notBlankErrorMessage);
+	}
+
+	@Test
+	void notNull인_필드에_null이_들어가면_예외를_발생한다() {
+		//given
+		Member mockMember = Member.builder()
+			.memberId(1L)
+			.email("test@dduikka.com")
+			.password("1234")
+			// .memberStatus(MemberStatus.JOIN)
+			.joinDate(LocalDate.now())
+			.createAt(LocalDateTime.now())
+			.build();
+
+		//when, then
+		assertThatThrownBy(() -> customValidator.validateObject(mockMember))
+			.isInstanceOf(IllegalStateException.class)
+			.hasMessageContaining(notNullErrorMessage);
+	}
+
+	@Test
+	void email_형식이_알맞지_않으면_예외를_발생한다() {
+		//given
+		Member mockMember = Member.builder()
+			.memberId(1L)
+			.email("test@")
+			.password("1234")
+			.memberStatus(MemberStatus.JOIN)
+			.joinDate(LocalDate.now())
+			.createAt(LocalDateTime.now())
+			.build();
+
+		//when, then
+		assertThatThrownBy(() -> customValidator.validateObject(mockMember))
+			.isInstanceOf(IllegalStateException.class)
+			.hasMessageContaining(emailErrorMessage);
+	}
+
+	@Test
+	void 과거나_현재날짜만_허용되는_필드가_미래일자면_예외를_발생한다() {
+		//given
+		LocalDate tomorrow = LocalDate.now().plusDays(1);
+		Member mockMember = Member.builder()
+			.memberId(1L)
+			.email("test@dduikka.com")
+			.password("1234")
+			.memberStatus(MemberStatus.JOIN)
+			.joinDate(tomorrow)
+			.createAt(LocalDateTime.now())
+			.build();
+
+		//when, then
+		assertThatThrownBy(() -> customValidator.validateObject(mockMember))
+			.isInstanceOf(IllegalStateException.class)
+			.hasMessageContaining(pastOrPresentErrorMessage);
 	}
 }
