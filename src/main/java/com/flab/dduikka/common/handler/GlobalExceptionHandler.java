74bfd7aa1b04951d3d09ce7968e7d3106d18e2e8
kimzoo2 <kimzoo1207@gmail.com>
2024-02-20 09:43:31 +0900

    Feat: Login 관련 exception 클래스 추가

--- src/main/java/com/flab/dduikka/common/handler/GlobalExceptionHandler.java
+++ src/main/java/com/flab/dduikka/common/handler/GlobalExceptionHandler.java
@@ -5,11 +5,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.flab.dduikka.domain.login.exception.LoginException;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(IllegalStateException.class)
 	public ResponseEntity<String> handleIllegalStateException(IllegalStateException e) {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+	}
+
+	@ExceptionHandler(LoginException.FailLoginException.class)
+	public ResponseEntity<String> handleFailLoginException(LoginException.FailLoginException e) {
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
 	}
 }
