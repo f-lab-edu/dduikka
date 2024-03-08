a311ccb738f9fffe33324f7d668f48baf9c9faeb
kimzoo2 <kimzoo1207@gmail.com>
2024-03-09 02:32:53 +0900

    Refactor: ResponseEntity 생성 책임 제거 및 @ResponseStatus 추가
    - ResponseEntity를 삭제하여 응답 상태값을 @ResponseStatus를 통하여 전달하도록 변경하였습니다.

--- src/main/java/com/flab/dduikka/domain/vote/api/VoteController.java
+++ src/main/java/com/flab/dduikka/domain/vote/api/VoteController.java
@@ -1,15 +1,15 @@
 package com.flab.dduikka.domain.vote.api;
 
-import java.net.URI;
 import java.time.LocalDate;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.flab.dduikka.domain.vote.application.VoteRecordService;
@@ -28,33 +28,28 @@ public class VoteController {
 	private final VoteRecordService voteRecordService;
 
 	@GetMapping("/{voteDate}")
-	public ResponseEntity<VoteResponseDto> findVote(@PathVariable final LocalDate voteDate) {
-		VoteResponseDto voteResponseDto = voteRecordService.findVoteTypeCount(voteDate);
-		return ResponseEntity.ok()
-			.body(voteResponseDto);
+	@ResponseStatus(HttpStatus.OK)
+	public VoteResponseDto findVote(@PathVariable final LocalDate voteDate) {
+		return voteRecordService.findVoteTypeCount(voteDate);
 	}
 
 	// TODO: USER 구현 후 userId 가져오는 방법 추가
 	@GetMapping("/record/{voteId}")
-	public ResponseEntity<VoteRecordResponseDto> findUserVoteRecord(@PathVariable final long voteId) {
+	@ResponseStatus(HttpStatus.OK)
+	public VoteRecordResponseDto findUserVoteRecord(@PathVariable final long voteId) {
 		long userId = 1L;
-		VoteRecordResponseDto voteRecordResponseDto = voteRecordService
-			.findUserVoteRecord(userId, voteId);
-		return ResponseEntity.ok()
-			.body(voteRecordResponseDto);
+		return voteRecordService.findUserVoteRecord(userId, voteId);
 	}
 
 	@PostMapping("/record")
-	public ResponseEntity<Void> addVoteRecord(@RequestBody @Valid final VoteRecordAddRequestDto request) {
+	@ResponseStatus(HttpStatus.CREATED)
+	public void addVoteRecord(@RequestBody @Valid final VoteRecordAddRequestDto request) {
 		voteRecordService.addVoteRecord(request);
-		return ResponseEntity.created(URI.create("/vote/record/" + LocalDate.now()))
-			.build();
 	}
 
 	@PatchMapping("/record/{voteRecordId}")
-	public ResponseEntity<Void> cancelVoteRecord(@PathVariable final long voteRecordId) {
+	@ResponseStatus(HttpStatus.OK)
+	public void cancelVoteRecord(@PathVariable final long voteRecordId) {
 		voteRecordService.cancelVoteRecord(voteRecordId);
-		return ResponseEntity.ok()
-			.build();
 	}
 }
