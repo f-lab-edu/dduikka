4138630aab225335712985adadd338009de6b010
kimzoo2 <kimzoo1207@gmail.com>
2024-03-08 21:07:22 +0900

    Fix: @Jsonformat으로 time의 직렬화 형식 적용

--- src/main/java/com/flab/dduikka/domain/livechat/dto/LiveChatResponse.java
+++ src/main/java/com/flab/dduikka/domain/livechat/dto/LiveChatResponse.java
@@ -1,7 +1,8 @@
 package com.flab.dduikka.domain.livechat.dto;
 
-import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.flab.dduikka.domain.livechat.domain.LiveChat;
 
 import lombok.AllArgsConstructor;
@@ -13,13 +14,14 @@ public class LiveChatResponse {
 
 	private long liveChatId;
 	private String text;
-	private String time;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+	private LocalDateTime time;
 
 	public static LiveChatResponse from(LiveChat liveChat) {
 		return new LiveChatResponse(
 			liveChat.getLiveChatId(),
 			liveChat.getMessage(),
-			DateTimeFormatter.ofPattern("HH:mm").format(liveChat.getCreatedAt())
+			liveChat.getCreatedAt()
 		);
 	}
 }
