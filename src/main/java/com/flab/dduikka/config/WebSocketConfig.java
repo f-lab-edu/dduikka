30114cc92ce466e78625ce513e8dc7c21d766f59
kimzoo2 <kimzoo1207@gmail.com>
2024-03-09 01:46:37 +0900

    Refactor: STOMP 설정 외부로 분리

--- src/main/java/com/flab/dduikka/config/WebSocketConfig.java
+++ src/main/java/com/flab/dduikka/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.flab.dduikka.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -12,16 +13,28 @@ import com.flab.dduikka.common.interceptor.CustomHttpSessionHandshakeInterceptor
 @EnableWebSocketMessageBroker   // STOMP 사용
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+	@Value("#{environment['websocket.broker.topic']}")
+	private String brokerTopic;
+
+	@Value("#{environment['websocket.broker.queue']}")
+	private String brokerQueue;
+
+	@Value("#{environment['websocket.app.destination.prefix']}")
+	private String appDestinationPrefix;
+
+	@Value("#{environment['websocket.endpoint']}")
+	private String endPoint;
+
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) { // 메모리 기반 message broker 활성화
-		registry.enableSimpleBroker("/topic", "/queue"); //구독경로 topic(모든 사용자), queue(특정)
-		registry.setApplicationDestinationPrefixes("/app"); //메세지 발송
+		registry.enableSimpleBroker(brokerTopic, brokerQueue); //구독경로 topic(모든 사용자), queue(특정)
+		registry.setApplicationDestinationPrefixes(appDestinationPrefix); //메세지 발송
 	}
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry
-			.addEndpoint("/chats")
+			.addEndpoint(endPoint)
 			.addInterceptors(new CustomHttpSessionHandshakeInterceptor())
 			.setAllowedOriginPatterns("*").withSockJS();
 	}
