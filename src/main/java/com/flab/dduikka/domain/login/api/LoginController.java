e0952ed6e4fb61f779e388486b137e445a998253
kimzoo2 <kimzoo1207@gmail.com>
2024-03-21 16:52:16 +0900

    Feat: 채팅 작성자 암호화 기능 추가 및 로그인 시 암호화된 식별자 쿠키 등록
    - 채팅 목록 조회 시, dto에서 메세지 작성자를 암호화하도록 변경하였습니다.
    - 클라이언트에서 암호화된 메세지 작성자를 비교할 수 있도록 쿠키에 등록하였습니다.

--- src/main/java/com/flab/dduikka/domain/login/api/LoginController.java
+++ src/main/java/com/flab/dduikka/domain/login/api/LoginController.java
@@ -6,12 +6,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.flab.dduikka.common.util.SHA256Encryptor;
 import com.flab.dduikka.domain.login.application.LoginService;
 import com.flab.dduikka.domain.login.dto.LoginRequestDto;
 import com.flab.dduikka.domain.login.dto.SessionMember;
 import com.flab.dduikka.domain.login.exception.LoginException;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -24,13 +27,21 @@ public class LoginController {
 
 	@PostMapping("/login")
 	@ResponseStatus(HttpStatus.OK)
-	public void login(@Valid @RequestBody LoginRequestDto loginRequestDto, HttpServletRequest request) {
+	public void login(
+		@Valid @RequestBody LoginRequestDto loginRequestDto,
+		HttpServletRequest request,
+		HttpServletResponse response
+	) {
 		SessionMember sessionMember = loginService.login(loginRequestDto);
 		if (sessionMember == null) {
 			throw new LoginException.FailLoginException("찾을 수 없는 회원입니다.");
 		}
 		HttpSession session = request.getSession(true);
 		session.setAttribute(SessionKey.LOGIN_USER.name(), sessionMember);
+		Cookie cookie =
+			new Cookie("EID", SHA256Encryptor.hashSHA256(String.valueOf(sessionMember.getMemberId())));
+		response.addCookie(cookie);
+
 	}
 
 	@PostMapping("/logout")
