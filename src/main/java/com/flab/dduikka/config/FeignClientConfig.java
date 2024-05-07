8872ec4def9d186ccaf6853e7582a215befe3761
kimzoo2 <kimzoo1207@gmail.com>
2024-05-07 17:24:17 +0900

    Refactor: FeignClient 스캔 범위 변경

--- src/main/java/com/flab/dduikka/config/FeignClientConfig.java
+++ src/main/java/com/flab/dduikka/config/FeignClientConfig.java
@@ -3,7 +3,9 @@ package com.flab.dduikka.config;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
+import com.flab.dduikka.domain.weather.application.AccuWeatherClient;
+
 @Configuration
-@EnableFeignClients(basePackages = "com.flab.dduikka")
+@EnableFeignClients(basePackageClasses = AccuWeatherClient.class)
 public class FeignClientConfig {
 }
