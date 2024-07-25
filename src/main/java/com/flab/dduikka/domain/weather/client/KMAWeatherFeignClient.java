16e14c8f027eb6464310307321cac37547bb0ea8
kimzoo2 <kimzoo1207@gmail.com>
2024-07-25 23:59:08 +0900

    Rename: Client 패키지 변경으로 인한 import 경로 변경 반영

--- src/main/java/com/flab/dduikka/domain/weather/client/KMAWeatherFeignClient.java
+++ src/main/java/com/flab/dduikka/domain/weather/client/KMAWeatherFeignClient.java
@@ -1,4 +1,4 @@
-package com.flab.dduikka.domain.weather.application;
+package com.flab.dduikka.domain.weather.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
