d5f47214910bcc31f82b12f3cf65020756061110
kimzoo2 <kimzoo1207@gmail.com>
2024-08-19 15:03:22 +0900

    Refactor: 컨트롤러 @ModelAttribute 활용하여 매핑하도록 변경
    - 컨트롤러 매핑 객체 변경
    - WeatherService 딜레이 시간 properties 속성 추가

--- src/main/java/com/flab/dduikka/domain/weather/api/WeatherController.java
+++ src/main/java/com/flab/dduikka/domain/weather/api/WeatherController.java
@@ -1,11 +1,12 @@
 package com.flab.dduikka.domain.weather.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.flab.dduikka.domain.weather.application.WeatherService;
+import com.flab.dduikka.domain.weather.dto.WeatherRequestDTO;
 import com.flab.dduikka.domain.weather.dto.WeatherResponseDTO;
 
 import lombok.RequiredArgsConstructor;
@@ -18,10 +19,7 @@ public class WeatherController {
 	private final WeatherService weatherService;
 
 	@GetMapping
-	public WeatherResponseDTO findWeather(
-		@RequestParam String latitude,
-		@RequestParam String longitude
-	) {
-		return weatherService.findWeather(latitude, longitude);
+	public WeatherResponseDTO findWeather(@ModelAttribute WeatherRequestDTO request) {
+		return weatherService.findWeather(request);
 	}
 }
--- src/main/java/com/flab/dduikka/domain/weather/application/WeatherService.java
+++ src/main/java/com/flab/dduikka/domain/weather/application/WeatherService.java
@@ -3,31 +3,44 @@ package com.flab.dduikka.domain.weather.application;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.flab.dduikka.domain.weather.client.WeatherClient;
 import com.flab.dduikka.domain.weather.domain.Weather;
+import com.flab.dduikka.domain.weather.dto.WeatherRequestDTO;
 import com.flab.dduikka.domain.weather.dto.WeatherResponseDTO;
 import com.flab.dduikka.domain.weather.exception.WeatherException;
 import com.flab.dduikka.domain.weather.repository.WeatherRepository;
 
 import feign.FeignException;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class WeatherService {
 
+	private final Integer advanceMinutes;
 	private final List<WeatherClient> weatherClients;
 	private final WeatherRepository weatherRepository;
 
-	public WeatherResponseDTO findWeather(String latitude, String longitude) {
-		String geoHash = pointToGeoHash(latitude, longitude);
-		Weather foundWeather = weatherRepository.findWeatherById(geoHash)
-			.orElseThrow(
-				() -> new WeatherException.WeatherNotFoundException("해당 위치에 대한 날씨가 존재하지 않습니다. weatherId: " + geoHash));
+	@Autowired
+	public WeatherService
+		(
+			@Value("${weather.advance-minutes}") Integer advanceMinutes,
+			List<WeatherClient> weatherClients,
+			WeatherRepository weatherRepository
+		) {
+		this.advanceMinutes = advanceMinutes;
+		this.weatherClients = weatherClients;
+		this.weatherRepository = weatherRepository;
+	}
+
+	public WeatherResponseDTO findWeather(WeatherRequestDTO request) {
+		String geoHash = pointToGeoHash(request.getLatitude(), request.getLongitude());
+		Weather foundWeather = weatherRepository.findWeatherById(geoHash).orElseThrow(
+			() -> new WeatherException.WeatherNotFoundException("해당 위치에 대한 날씨가 존재하지 않습니다. weatherId: " + geoHash));
 		return WeatherResponseDTO.from(foundWeather);
 	}
 
@@ -41,6 +54,17 @@ public class WeatherService {
 			}
 		}
 		throw new WeatherException.ExternalAPIException("외부 API에서 오류가 발생하였습니다.");
+	}
+
+	/**
+	 * 기상청 api 기준으로 발표 시간 딜레이가 존재한다.
+	 * n분을 앞당겨 발표가 딜레이 되어도 조회할 수 있도록 변경한다.
+	 *
+	 * @param requestTime
+	 * @return LocalDateTime
+	 */
+	private LocalDateTime advanceForecastRequestTime(LocalDateTime requestTime) {
+		return requestTime.minusMinutes(advanceMinutes);
 	}
 
 	//TODO : weather API 갱신하기 구현 시에 geohash 값 얻어오도록 함
