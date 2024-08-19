a6b67209d2e0ea56a98d8bb22b73173387f1f09e
kimzoo2 <kimzoo1207@gmail.com>
2024-08-19 15:03:47 +0900

    Test: Weather 조회 test 변경 및 추가

--- src/test/java/com/flab/dduikka/domain/weather/api/WeatherDocumentationTest.java
+++ src/test/java/com/flab/dduikka/domain/weather/api/WeatherDocumentationTest.java
@@ -8,8 +8,6 @@ import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import java.time.LocalDateTime;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
@@ -17,6 +15,7 @@ import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import com.flab.dduikka.domain.helper.ApiDocumentationHelper;
 import com.flab.dduikka.domain.location.domain.Location;
 import com.flab.dduikka.domain.weather.domain.Weather;
+import com.flab.dduikka.domain.weather.dto.WeatherRequestDTO;
 import com.flab.dduikka.domain.weather.dto.WeatherResponseDTO;
 
 class WeatherDocumentationTest extends ApiDocumentationHelper {
@@ -25,39 +24,31 @@ class WeatherDocumentationTest extends ApiDocumentationHelper {
 	@DisplayName("날씨를 조회한다")
 	void findWeather() throws Exception {
 		// given
-		LocalDateTime requestDateTime = LocalDateTime.now();
-		String latitude = "55";
-		String longitude = "127";
-		String cityCode = "";
+		WeatherRequestDTO request = new WeatherRequestDTO("55", "127");
 		Weather response =
 			Weather.builder()
-				.forecastDateTime(requestDateTime)
-				.location(new Location(latitude, longitude))
+				.location(new Location(request.getLatitude(), request.getLongitude()))
 				.temperature(15.3)
 				.snowfall(0.0)
 				.rainfall(0.0)
 				.relativeHumidity(61)
 				.build();
-		given(weatherService.getWeather(any(), anyString(), anyString(), anyString()))
+		given(weatherService.findWeather(any()))
 			.willReturn(WeatherResponseDTO.from(response));
 
 		// when
 		mockMvc.perform(
 				RestDocumentationRequestBuilders
-					.get("/weathers?dateTime={dateTime}&latitude={latitude}&longitude={longitude}&cityCode={cityCode}",
-						requestDateTime,
-						latitude,
-						longitude,
-						cityCode))
+					.get("/weathers?latitude={latitude}&longitude={longitude}",
+						request.getLatitude(),
+						request.getLongitude()))
 			.andDo(print())
 			.andDo(document("weathers/findWeather",
 				preprocessRequest(prettyPrint()),
 				preprocessResponse(prettyPrint()),
 				queryParameters(
-					parameterWithName("dateTime").description("요청일시 ex) yyyy-MM-dd HH:mm:ss.SSS"),
 					parameterWithName("latitude").description("위도"),
-					parameterWithName("longitude").description("경도"),
-					parameterWithName("cityCode").description("도시코드")
+					parameterWithName("longitude").description("경도")
 				),
 				responseFields(
 					fieldWithPath("headers").description("헤더"),
---
+++ src/test/java/com/flab/dduikka/domain/weather/repository/JdbcWeatherRepositoryTest.java
@@ -0,0 +1,70 @@
+package com.flab.dduikka.domain.weather.repository;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.flab.dduikka.domain.helper.JDBCRepositoryTestHelper;
+import com.flab.dduikka.domain.location.domain.Location;
+import com.flab.dduikka.domain.weather.domain.Weather;
+
+class JdbcWeatherRepositoryTest extends JDBCRepositoryTestHelper {
+
+	@Autowired
+	private WeatherRepository weatherRepository;
+
+	@Test
+	@DisplayName("날씨를 등록한다.")
+	void addWeather() {
+		// given
+		LocalDateTime forecastDate = LocalDateTime.of(2024, 07, 25, 21, 12);
+		Weather mockWeather =
+			Weather.builder()
+				.weatherId("abcdedf")
+				.forecastDateTime(forecastDate)
+				.location(new Location("37", "125"))
+				.temperature(33.0)
+				.relativeHumidity(70)
+				.rainfall(0.0)
+				.snowfall(0.0)
+				.createdAt(forecastDate)
+				.build();
+
+		// when
+		weatherRepository.addWeather(mockWeather);
+
+		// then
+		Weather foundWeather = weatherRepository.findWeatherById(mockWeather.getWeatherId()).get();
+		assertThat(mockWeather).isEqualTo(foundWeather);
+	}
+
+	@Test
+	@DisplayName("날씨를 조회한다.")
+	void findWeather() {
+		// given
+		LocalDateTime forecastDate = LocalDateTime.of(2024, 07, 25, 21, 12);
+		Weather mockWeather =
+			Weather.builder()
+				.weatherId("abcdedf")
+				.forecastDateTime(forecastDate)
+				.location(new Location("37", "125"))
+				.temperature(33.0)
+				.relativeHumidity(70)
+				.rainfall(0.0)
+				.snowfall(0.0)
+				.createdAt(forecastDate)
+				.build();
+		weatherRepository.addWeather(mockWeather);
+
+		// when
+		Weather foundWeather = weatherRepository.findWeatherById(mockWeather.getWeatherId()).get();
+
+		// then
+		assertThat(foundWeather).isEqualTo(mockWeather);
+	}
+
+}
